c9bf828382231b229954923eedda6c7ba754808d
RaulMirandaOSF <raul.miranda@osf-commerce.com>
2019-03-08 10:11:03 -0300

    Doing correctly pagination

--- js/script.js
+++ js/script.js
@@ -5,58 +5,68 @@ FSJS project 2 - List Filter and Pagination
    
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
+var numberStudentPerPage = 10;
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+// Goes through all students to set them visible or not
 
+function showPage(pageNumber) {
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
+   var students = document.querySelectorAll('body > div > ul > li');
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+   pageNumber--;
 
-var students = document.querySelectorAll('body > div > ul > li');
-
-function showPage(students, pageNumber) {
-
-   // Delete all students
    for (var i = 0; i < students.length; i++) {
-      students[i].style.display = 'none';
-   }
 
-   // Show only selected students
-   // document.querySelector('body > div > ul > li:nth-child(1)').style.display = 'inherit'
-   
+      if (i >= pageNumber * numberStudentPerPage  && i < (pageNumber + 1) * numberStudentPerPage) {
+         students[i].style.display = 'block';
+      } else {
+         students[i].style.display = 'none';
+      }
+   }
 }
 
-showPage(students);
+// Append links to do pagination (1, 2, ...)
 
+function appendPageLinks() {
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+   var page = document.querySelector('.page');
+   var students = document.querySelectorAll('body > div > ul > li');
 
+   // Go to default page = 1
+   showPage(1);
+   
+   var paginationHTML = '<div class="pagination"><ul>';
+   
+   // Activate/Desactivate pagination link
+   for (let i = 1; i < ((students.length / numberStudentPerPage) + 1); i++) {
+      if (i == 1) {
+         paginationHTML += '<li><a class="active" href="#">' + i + '</a></li>';
+      } else {
+         paginationHTML += '<li><a href="#">' + i + '</a></li>';
+      }
+   }
 
+   paginationHTML += '</ul></div>';
+   page.innerHTML += paginationHTML;
 
+   var pagination = document.querySelectorAll('body > div > div.pagination > ul > li');
 
+   for (let j = 0; j < pagination.length; j++) {
+      const clickedLink = pagination[j].getElementsByTagName('a')[0];
+      
+      clickedLink.addEventListener('click', function() {
+         
+         // Active only the clicked link and desactive others
+         for (let k = 0; k < pagination.length; k++) {
+            const link = pagination[k].getElementsByTagName('a')[0];
+            link.classList.remove("active");
+         }
+         
+         clickedLink.classList.add("active");
+         let pageNumber = parseInt(clickedLink.textContent);
+         showPage(pageNumber);
+      });
+   }
+}
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+appendPageLinks();
